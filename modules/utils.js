2ef4d0a6b955484a7ede2aedb21f5806230a191a
joara613 <joara613@gmail.com>
2023-02-08 23:49:13 -0700

    add header effect/loading page and fix horizontal scrollbar

--- modules/utils.js
+++ modules/utils.js
@@ -53,18 +53,64 @@ export function fadeOutEffect(targetEl) {
 			targetEl.style.opacity = 1;
 		}
 		if (targetEl.style.opacity > 0) {
-			targetEl.style.opacity -= 0.01;
+			targetEl.style.opacity -= 0.02;
 		} else {
 			clearInterval(fadeEffect);
-			targetEl.style.display = none;
+			targetEl.style.zIndex = "-1";
 		}
 	}, 20);
 }
 
+function headerHandler() {
+	let doc = document.documentElement;
+
+	let prevScroll = scrollY || doc.scrollTop;
+	let curScroll;
+	let direction = 0;
+	let prevDirection = 0;
+
+	let checkScroll = function () {
+		/*
+		 ** Find the direction of scroll
+		 ** 0 - initial, 1 - up, 2 - down
+		 */
+
+		curScroll = scrollY || doc.scrollTop;
+		if (curScroll > prevScroll) {
+			//scrolled up
+			direction = 2;
+		} else if (curScroll < prevScroll) {
+			//scrolled down
+			direction = 1;
+		}
+
+		if (direction !== prevDirection) {
+			toggleHeader(direction, curScroll);
+		}
+
+		prevScroll = curScroll;
+	};
+
+	let toggleHeader = function (direction, curScroll) {
+		if (direction === 2 && curScroll > 90) {
+			header.classList.add("hide");
+			prevDirection = direction;
+		} else if (direction === 1) {
+			header.classList.remove("hide");
+			prevDirection = direction;
+		}
+	};
+
+	window.addEventListener("scroll", checkScroll);
+}
+
 // implements----------------------------------
+
+// wave text
 let text = document.querySelector(".no-experience-text").innerHTML;
 waveText(text);
 
+// scroll Handler
 let navItems = document.querySelectorAll(".nav-item");
 
 navItems.forEach((navItem) => {
@@ -73,9 +119,31 @@ navItems.forEach((navItem) => {
 	});
 });
 
-// let loadingEl = document.getElementById("loading-page");
+// Loading page fadeout
+let loadingEl = document.getElementById("loading-page");
 
-// window.onload = (event) => {
-// 	console.log("page is fully loaded");
-// 	fadeOutEffect(loadingEl);
-// };
+window.onload = (event) => {
+	setTimeout(() => {
+		fadeOutEffect(loadingEl);
+	}, 1000);
+};
+
+// header disappears when scrolling down
+let header = document.getElementById("site-header");
+let headerBackground = document.querySelector(".star-background");
+
+setTimeout(() => {
+	headerHandler();
+}, 200);
+
+addEventListener("scroll", (event) => {
+	console.log(scrollY);
+	if (scrollY + 500 > innerHeight) {
+		header.style.opacity = "1";
+		headerBackground.classList.add("visible");
+	} else {
+		header.style.opacity = "0.5";
+		headerBackground.classList.remove("visible");
+	}
+	console.log(headerBackground);
+});
