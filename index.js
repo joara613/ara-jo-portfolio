eacf2e6f0a7a3033ff0200c6d0090605b4282eb3
joara613 <joara613@gmail.com>
2023-02-21 20:27:46 -0700

    add joker teller(ver. w/o voiceRSS)

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 require("dotenv").config();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+
+// Set static folder
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 	res.render("pages/main-page");
@@ -20,81 +22,6 @@ app.get("/music-player", function (req, res) {
 app.get("/joke-teller", function (req, res) {
 	res.render("pages/joke-teller");
 });
-
-// app
-// 	.route("/")
-// 	.get((req, res) => {
-// 		res.sendFile("/index.html");
-// 	})
-// 	.post(async (req, res) => {
-//SMTP transport
-// 1
-// const transporter = nodemailer.createTransport({
-// 	port: 465, // true for 465, false for other ports
-// 	host: "smtp.gmail.com",
-// 	auth: {
-// 		type: "OAuth2",
-// 		user: "joara613@gmail.com",
-// 		clientId: process.env.ID,
-// 		clientSecret: process.env.SECRET,
-// 	},
-// 	secure: true,
-// });
-// 2
-// const transporter = nodemailer.createTransport({
-// 	service: "gmail",
-// 	auth: {
-// 		user: process.env.ID, // your email address
-// 		pass: process.env.PASS, // your email password
-// 	},
-// });
-
-// const mailData = {
-// 	from: "joara613@gmail.com", // sender address
-// 	to: "joara613@gmail.com", // list of receivers
-// 	subject: "[nodemailer]" + req.body.name + " : " + req.body.subject,
-// 	// text: req.body.content,
-// 	html: `${req.body.email}</br>${req.body.content}`,
-// 	generateTextFromHTML: true,
-// };
-
-// const accessToken = await oAuth2Client.getAccessToken();
-
-// const transporter = nodemailer.createTransport({
-// 	service: "gmail",
-// 	auth: {
-// 		type: "OAuth2",
-// 		user: "joara613@gmail.com",
-// 		clientId: process.env.ID,
-// 		clientSecret: process.env.SECRET,
-// 		accessToken: accessToken.token,
-// 	},
-// });
-
-// let transporter = nodemailer.createTransport({
-// 	host: "smtp.gmail.com",
-// 	port: 465,
-// 	secure: true,
-// 	auth: {
-// 		type: "OAuth2",
-// 		user: process.env.EMAIL,
-// 	},
-// });
-
-// transporter.set("oauth2_provision_cb", (user, renew, callback) => {
-// 	let accessToken = userTokens[user];
-// 	if (!accessToken) {
-// 		return callback(new Error("Unknown user"));
-// 	} else {
-// 		return callback(null, accessToken);
-// 	}
-// });
-
-// 	transporter.sendMail(mailData, function (err, info) {
-// 		if (err) console.log(err);
-// 		else console.log(info);
-// 	});
-// });
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`Example app listening on port 3000`);
--- public/scripts/joke-teller/index.js
+++ public/scripts/joke-teller/index.js
@@ -5,19 +5,6 @@ const audioElement = document.getElementById("audio");
 function toggleButton() {
 	button.disabled = !button.disabled;
 }
-// Passing Joke to VoiceRSS API
-function tellMe(joke) {
-	VoiceRSS.speech({
-		key: environment["VoiceAPIKey"],
-		src: joke,
-		hl: "en-us",
-		v: "Linda",
-		r: 0,
-		c: "mp3",
-		f: "44khz_16bit_stereo",
-		ssml: false,
-	});
-}
 
 // Get Jokes from Joke API
 async function getJokes() {
@@ -32,8 +19,10 @@ async function getJokes() {
 		} else {
 			joke = data.joke;
 		}
-		// Text-to-Speech
-		tellMe(joke);
+		msg = new SpeechSynthesisUtterance(joke);
+		msg.addEventListener("end", toggleButton);
+		window.speechSynthesis.speak(msg);
+
 		// Disalbe Button
 		toggleButton();
 	} catch (error) {
@@ -44,4 +33,3 @@ async function getJokes() {
 
 // Event Listeners
 button.addEventListener("click", getJokes);
-audioElement.addEventListener("ended", toggleButton);
